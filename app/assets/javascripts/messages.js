c176bc43ca01e620b7818163e2d8712cf44ca20c
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-02-08 11:07:45 -0500

    have date show in ajax post

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -10,8 +10,18 @@ $(function(){
       var commentFullDiv = $('<div>').addClass('fullMessage');
       var commentDiv = $('<p>').addClass('comment')
                              .append(returnData.text);
-      var commentorDiv = $('<div>').addClass('guest-name')
-                                 .append(returnData.guestname);
+      var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      var returnDate = new Date();
+      var month = months[returnDate.getMonth()];
+      var date = returnDate.getDate();
+      var year = returnDate.getFullYear();
+      var getHour = returnDate.getHours();
+      var hour = (getHour>12)? getHour-12:getHour;
+      var minute = (returnDate.getMinutes()<10)? "0"+returnDate.getMinutes():returnDate.getMinutes();
+      var ampm = (getHour>12)? "PM":"AM";
+
+      var commentorDiv = $('<div>').addClass('guest-name');
+      commentorDiv.append(`${returnData.guestname} wrote on ${month} ${date}, ${year} at ${hour}:${minute} ${ampm}`);
       commentFullDiv.append(commentDiv).append(commentorDiv);
       $('#messageBoard').prepend(commentFullDiv);
     }).done(function(){
